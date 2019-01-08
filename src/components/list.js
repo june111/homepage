a45d9e5d2bfc258d4c17239283b0555fc1d39d9d
June <june@linktimetech.com>
2019-01-08 17:38:12 +0800

    init

---
+++ src/components/list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const name = 'June';
+const element = <div>About {name} </div>;
+const About = () => {
+	return (
+		element
+	)
+};
+
+export default About;
---
+++ src/pages/App.js
@@ -0,0 +1,23 @@
+import React, {
+  Component
+} from 'react';
+import {
+  BrowserRouter
+} from 'react-router-dom'
+
+import {
+   Main
+} from '../components/main';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    );
+
+  }
+}
+
+export default App;
