04879a00aa31fd06dc1fd909b60293aa09571e7c
June <yula.qian@gmail.com>
2019-01-15 20:06:10 +0800

    update path

---
+++ docs/precache-manifest.75c56ad88a241b606ac0fe4e74a1d910.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9f2de54d44bb9049b00",
+    "url": "/homepage/static/js/runtime~main.c9f2de54.js"
+  },
+  {
+    "revision": "ad4579e110b8395d6217",
+    "url": "/homepage/static/js/main.ad4579e1.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/homepage/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "79dbbcc497d4f0bb2fe75478ef70d768",
+    "url": "/homepage/index.html"
+  }
+];
